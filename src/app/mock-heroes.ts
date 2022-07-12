da16600b09da113d36e1c139a57e92a9d8a451e2
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-07-12 12:54:11 +0200

    Create a mock array of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'All Might' },
+    { id: 12, name: 'Deku' },
+    { id: 13, name: 'Naruto' },
+    { id: 14, name: 'Ichigo' },
+    { id: 15, name: 'Guts' },
+    { id: 16, name: 'Goku' },
+    { id: 17, name: 'Superman' },
+    { id: 18, name: 'Shisui' },
+    { id: 19, name: 'Nagato' },
+    { id: 20, name: 'Madara Uchiha' },
+];
