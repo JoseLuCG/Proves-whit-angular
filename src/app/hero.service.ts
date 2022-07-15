f135e556ead14705bae031553fe132d98396e890
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-07-15 09:47:33 +0200

    Solve the conflict again.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,11 +12,7 @@ export class HeroService {
   constructor(private messageService: MessageService) { }
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
-<<<<<<< HEAD
-    this.messageService.add('HeroService: feched heroes');
-=======
     this.messageService.add('HeroService: fetched heroes');
->>>>>>> 30f2aa29f687f40b3c7b50b28431f7167990369f
     return heroes;
   }
 }
