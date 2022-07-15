85cd6299dca027dc6efbc07825a9f6251f31d752
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-07-15 13:42:52 +0200

    Add the method log.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
 
@@ -9,7 +10,9 @@ import { MessageService } from './message.service';
 @Injectable({providedIn: 'root'})
 export class HeroService {
 
-  constructor(private messageService: MessageService) {}
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) {}
   
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
@@ -20,5 +23,8 @@ export class HeroService {
     const hero = HEROES.find(h => h.id === id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
+  }
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
