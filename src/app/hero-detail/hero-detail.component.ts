4f864a4cbc90d578b4e861be4af6f6f1ff70631e
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-07-18 12:23:37 +0200

    Add the new method <save()> for save the changes at the server.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -31,4 +31,10 @@ hero: Hero | undefined;
   goBack():void {
     this.location.back();
   }
+  save(): void {
+    if (this.hero){
+      this.heroService.updateHero(this.hero)
+        .subscribe(()=> this.goBack());
+    }
+  }
 }
