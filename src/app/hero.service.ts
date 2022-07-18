08272abba203f0f66dec340a9b338882821fe931
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-07-18 09:35:03 +0200

    Change the method <getHeroes()> for use the http client.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,15 +9,14 @@ import { MessageService } from './message.service';
 
 @Injectable({providedIn: 'root'})
 export class HeroService {
+  private heroesUrl = 'api/heroes';
 
   constructor(
     private http: HttpClient,
     private messageService: MessageService) {}
   
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
   getHero(id:number): Observable<Hero> {
     const hero = HEROES.find(h => h.id === id)!;
